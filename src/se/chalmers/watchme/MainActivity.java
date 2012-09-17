878ea6dbd6c8c18243858a85c570450fe1e7dedc
Johan Brook <johan.jvb@gmail.com>
2012-09-17 20:28:58 +0200

    Add barebones list view to MainActivity.
    MainActivity now contains a simple list view filled with an
    ArrayList of three movies.

--- src/se/chalmers/watchme/MainActivity.java
+++ src/se/chalmers/watchme/MainActivity.java
@@ -1,15 +1,30 @@
 package se.chalmers.watchme;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+	
+	private List<String> list;
+	private ArrayAdapter<String> moviesAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        String[] strings = {"The Lord Of The Rings", "The Godfather", "Star Wars"};
+        list = new ArrayList<String>(Arrays.asList(strings));
+        
+        moviesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        setListAdapter(moviesAdapter);
     }
 
     @Override
