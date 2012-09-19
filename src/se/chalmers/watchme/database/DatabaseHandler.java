be25be5e1aa9f72ee63137182117f8aad2438cba
Lisa Stenberg <lisast@student.chalmers.se>
2012-09-19 23:02:56 +0200

    Create and Read-methods for Movie-table implemented in the DatabaseHandler

--- src/se/chalmers/watchme/database/DatabaseHandler.java
+++ src/se/chalmers/watchme/database/DatabaseHandler.java
@@ -1,6 +1,12 @@
 package se.chalmers.watchme.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import se.chalmers.watchme.model.Movie;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -36,5 +42,61 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 		onCreate(db);
 	}
 	
+	/**
+	 * Adds a Movie to the database.
+	 * @param movie The Movie you want to add.
+	 */
+	public void addMovie(Movie movie) {
+		SQLiteDatabase db = this.getWritableDatabase();
+		 
+		//Is the ID inserted automatic?
+	    ContentValues values = new ContentValues();
+	    values.put(KEY_TITLE, movie.getTitle()); // Contact Name
+	    values.put(KEY_RATING, movie.getRating()); // Contact Phone Number
+	    values.put(KEY_NOTE, movie.getNote());
+	 
+	    // Inserting Row
+	    db.insert(TABLE_MOVIES, null, values);
+	    db.close(); // Closing database connection
+	}
 	
+	/**
+	 * @param id The unique id of the Movie
+	 * @return the Movie asked for.
+	 */
+	public Movie getMovie(int id) {
+		SQLiteDatabase db = this.getReadableDatabase();
+
+		Cursor cursor = db.query(TABLE_MOVIES, new String[] { KEY_ID,
+				KEY_TITLE, KEY_RATING, KEY_NOTE }, KEY_ID + "=?",
+				new String[] { String.valueOf(id) }, null, null, null, null);
+		if (cursor != null)
+			cursor.moveToFirst();
+
+		Movie movie = new Movie(cursor.getString(0), Integer.parseInt(cursor.getString(1)), cursor.getString(2));
+		return movie;
+	}
+	
+	/**
+	 * @return All Movies stored in the database.
+	 */
+	public List<Movie> getAllMovies() {
+		List<Movie> allMovies = new ArrayList<Movie>();
+
+		String selectQuery = "SELECT * FROM " + TABLE_MOVIES;
+
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		if (cursor.moveToFirst()) {
+			do {
+				Movie movie = new Movie(cursor.getString(0),
+						Integer.parseInt(cursor.getString(1)),
+						cursor.getString(2));
+				allMovies.add(movie);
+			} while (cursor.moveToNext());
+		}
+		
+		return allMovies;
+	}
 }
