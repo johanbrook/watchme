7c0a3f263ba92defb1a111032f1edf2b771a5bd4
Johan Brook <johan.jvb@gmail.com>
2012-09-24 22:50:13 +0200

    Fix incorrect name of start command callback

--- src/se/chalmers/watchme/notifications/NotificationService.java
+++ src/se/chalmers/watchme/notifications/NotificationService.java
@@ -12,6 +12,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 public class NotificationService extends Service {
 	
@@ -28,13 +29,16 @@ public class NotificationService extends Service {
 		return this.binder;
 	}
 	
-	public int startCommand(Intent intent, int flags, int startID) {
-
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startID) {
+		Log.i("Custom", "Received start id " + startID + ": " + intent);
+				
 		// This service is sticky - it runs until it's stopped.
 		return START_STICKY;
 	}
 	
 	public void setAlarm(Calendar c) {
+		Log.i("Custom", "Set alarm");
 		// Start a new task for the alarm on another thread (separated from the UI thread)
 		new AlarmTask(this, c).run();
 	}
