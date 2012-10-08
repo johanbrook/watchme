7b364ac4f6c21a33e139e6de3495efe51ce309a1
Johan Brook <johan.jvb@gmail.com>
2012-10-09 00:19:49 +0200

    Add minutes to human helper function

--- src/se/chalmers/watchme/utils/DateTimeUtils.java
+++ src/se/chalmers/watchme/utils/DateTimeUtils.java
@@ -11,6 +11,8 @@ import java.util.Calendar;
  */
 public class DateTimeUtils {
 	
+	private final static int HOUR = 60;
+	
 	/**
 	 * Recieves a calendar instance and returns a String with simple
 	 * date format(dd-MM-yyyy)
@@ -25,6 +27,19 @@ public class DateTimeUtils {
 		
 		return simpleDateString;
 		
+	}
+	
+	/**
+	 * Convert duration in minutes to a more human readable form.
+	 * 
+	 * @param minutes The duration in minutes
+	 * @return A string on the format "HH:mm"
+	 */
+	public static String minutesToHuman(int minutes) {
+		int remainingMinutes = minutes % HOUR;
+		int hours = minutes / HOUR;
+		
+		return hours+":" + ((remainingMinutes < 10) ? "0"+remainingMinutes : remainingMinutes);
 	}
 	
 	
