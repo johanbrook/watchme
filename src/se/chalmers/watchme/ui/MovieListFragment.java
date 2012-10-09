8a01236f4264875bf50123d0fb19badce924258f
Johan Brook <johan.jvb@gmail.com>
2012-10-09 23:04:16 +0200

    Implement movie posters in movie list view

--- src/se/chalmers/watchme/ui/MovieListFragment.java
+++ src/se/chalmers/watchme/ui/MovieListFragment.java
@@ -1,13 +1,21 @@
 package se.chalmers.watchme.ui;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import se.chalmers.watchme.R;
 import se.chalmers.watchme.activity.MovieDetailsActivity;
 import se.chalmers.watchme.database.MoviesTable;
 import se.chalmers.watchme.database.WatchMeContentProvider;
 import se.chalmers.watchme.model.Movie;
+import se.chalmers.watchme.net.IMDBHandler;
 import se.chalmers.watchme.utils.DateTimeUtils;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
@@ -17,7 +25,10 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.text.format.DateFormat;
@@ -30,6 +41,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,8 +58,19 @@ public class MovieListFragment extends ListFragment implements LoaderManager.Loa
 		super.onActivityCreated(b);
 		Thread.currentThread().setContextClassLoader(getActivity().getClassLoader());
 
-		String[] from = new String[] { MoviesTable.COLUMN_MOVIE_ID, MoviesTable.COLUMN_TITLE,  MoviesTable.COLUMN_RATING , MoviesTable.COLUMN_DATE };
-		int[] to = new int[] { 0 , R.id.title, R.id.raiting, R.id.date };
+		String[] from = new String[] { 
+				MoviesTable.COLUMN_MOVIE_ID, 
+				MoviesTable.COLUMN_TITLE,  
+				MoviesTable.COLUMN_RATING ,
+				MoviesTable.COLUMN_DATE,
+				MoviesTable.COLUMN_IMDB_ID
+				};
+		
+		int[] to = new int[] { 0 , 
+				R.id.title, 
+				R.id.raiting, 
+				R.id.date,
+				R.id.poster};
 		
 		getActivity().getLoaderManager().initLoader(0, null, this);
 		adapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item_movie , null, from, to, 0);
@@ -59,7 +82,8 @@ public class MovieListFragment extends ListFragment implements LoaderManager.Loa
 		adapter.setViewBinder(new ViewBinder() {
 			
 			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-				if (columnIndex == 3) {
+				
+				if (columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_DATE)) {
 					String date = cursor.getString(columnIndex);
 					TextView textView = (TextView) view;
 					Calendar cal = Calendar.getInstance();
@@ -67,6 +91,13 @@ public class MovieListFragment extends ListFragment implements LoaderManager.Loa
 					String formattedDate = DateTimeUtils.toSimpleDate(cal);
 					
 					textView.setText(formattedDate);
+					return true;
+				}
+				else if(columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_IMDB_ID)) {
+					int apiID = cursor.getInt(columnIndex);
+					
+					new ImageDownloadTask((ImageView) view).execute(new Integer[] {apiID});
+					
 					return true;
 				}
 				
@@ -87,7 +118,13 @@ public class MovieListFragment extends ListFragment implements LoaderManager.Loa
 	}
 	
 	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-		String[] projection = { MoviesTable.COLUMN_MOVIE_ID, MoviesTable.COLUMN_TITLE, MoviesTable.COLUMN_RATING, MoviesTable.COLUMN_DATE };
+		String[] projection = { 
+				MoviesTable.COLUMN_MOVIE_ID,
+				MoviesTable.COLUMN_TITLE, 
+				MoviesTable.COLUMN_RATING, 
+				MoviesTable.COLUMN_DATE,
+				MoviesTable.COLUMN_IMDB_ID};
+		
 	    CursorLoader cursorLoader = new CursorLoader(getActivity(),
 	        uri, projection, null, null, null);
 	    return cursorLoader;
@@ -102,6 +139,65 @@ public class MovieListFragment extends ListFragment implements LoaderManager.Loa
 	    adapter.swapCursor(null);
 		
 	}
+	
+	/**
+     * Async task for downloading the movie's poster.
+     * 
+     * @author Johan
+     *
+     */
+    private class ImageDownloadTask extends AsyncTask<Integer, Void, Bitmap> {
+
+    	private ImageView view;
+    	
+    	public ImageDownloadTask(ImageView view) {
+    		this.view = view;
+    	}
+    	
+		@Override
+		protected Bitmap doInBackground(Integer... params) {
+			InputStream in = null;
+			String url = null;
+			JSONObject response = new IMDBHandler().getMovieById(params[0]);
+			JSONArray posters = response.optJSONArray("posters");
+	    	
+	    	if(posters != null && posters.length() > 0) {
+	    		
+	    		for(int i = 0; i < posters.length(); i++) {
+	    			JSONObject image = posters.optJSONObject(i).optJSONObject("image");
+	    			if(image.optString("size").equals("mid")) {
+	    				url = image.optString("url");
+	    				break;
+	    			}
+	    		}
+	    	}
+	    	
+	    	if(url == null) {
+	    		return null;
+	    	}
+			
+			try {
+				in = (InputStream) new URL(url).getContent();
+			} catch (MalformedURLException e) {
+				Log.e(getClass().getSimpleName(), "Bad URL format for poster");
+				e.printStackTrace();
+			} catch (IOException e) {
+				Log.e(getClass().getSimpleName(), "Error encoding image from URL");
+				e.printStackTrace();
+			}
+			
+			return BitmapFactory.decodeStream(in);
+		}
+		
+		@Override
+		protected void onPostExecute(Bitmap bm) {
+			if(bm != null) {
+				this.view.setImageBitmap(bm);
+			}
+		}
+    	
+    }
+	
 	
 	/**
      * Listener for when the user clicks an item in the list
@@ -153,7 +249,7 @@ public class MovieListFragment extends ListFragment implements LoaderManager.Loa
      * 
      * @author Johan
      */
-    public class OnDeleteListener implements OnItemLongClickListener {
+    private class OnDeleteListener implements OnItemLongClickListener {
     	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
 			final long movieId = id;
