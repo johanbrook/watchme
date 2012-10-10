dbe582f16b8cd5ce6de44bbc0a374349fd66008e
Lisa Stenberg <lisast@student.chalmers.se>
2012-10-10 10:33:42 +0200

    Started implementing shell for DatabaseAdapter

---
+++ src/se/chalmers/watchme/database/DatabaseAdapter.java
@@ -0,0 +1,41 @@
+package se.chalmers.watchme.database;
+
+import java.util.List;
+
+import se.chalmers.watchme.model.Movie;
+import se.chalmers.watchme.model.Tag;
+
+public class DatabaseAdapter {
+	
+	public Movie getMovie(int id) {
+		return null;
+	}
+	
+	public void addMovie(Movie movie) {
+		
+	}
+	
+	public void deleteMovie(Movie movie) {
+		
+	}
+	
+	public List<Movie> getMovies() {
+		return null;
+	}
+	
+	public Tag getTag(int id) {
+		return null;
+	}
+	
+	public void addTag(Tag tag) {
+		
+	}
+	
+	public void deleteTag(Tag tag) {
+		
+	}
+	
+	public List<Tag> getTags() {
+		return null;
+	}
+}
