d281bd30dcb1d229295bb841e1d4e55b83466839
Johan Brook <johan.jvb@gmail.com>
2012-10-10 14:26:09 +0200

    Add image URL fields to Movie database table

--- src/se/chalmers/watchme/database/MoviesTable.java
+++ src/se/chalmers/watchme/database/MoviesTable.java
@@ -17,11 +17,14 @@ public class MoviesTable {
 	public static final String COLUMN_NOTE = "note";
 	public static final String COLUMN_DATE = "releasedate";
 	public static final String COLUMN_IMDB_ID = "imdbid";
+	public static final String COLUMN_POSTER_LARGE = "poster_url_large";
+	public static final String COLUMN_POSTER_SMALL = "poster_url_small";
 
 	private static final String CREATE_MOVIES_TABLE = "CREATE TABLE "
 			+ TABLE_MOVIES + "(" + COLUMN_MOVIE_ID + " INTEGER PRIMARY KEY,"
 			+ COLUMN_TITLE + " TEXT," + COLUMN_RATING + " INTEGER," + COLUMN_NOTE
-			+ " TEXT," + COLUMN_DATE + " INTEGER," + COLUMN_IMDB_ID +" TEXT" + ")";
+			+ " TEXT," + COLUMN_DATE + " INTEGER," + COLUMN_IMDB_ID +" TEXT," 
+			+ COLUMN_POSTER_LARGE + " TEXT," + COLUMN_POSTER_SMALL+ " TEXT)";
 
 	public static void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_MOVIES_TABLE);
