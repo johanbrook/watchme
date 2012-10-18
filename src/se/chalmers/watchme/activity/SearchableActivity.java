3e22c100bd5cac69895a38a0f48736ef46fb6a5e
Lisa Stenberg <lisast@student.chalmers.se>
2012-10-18 10:34:23 +0200

    Implemented necessary methods for receiving the query in SearchableActivity

--- src/se/chalmers/watchme/activity/SearchableActivity.java
+++ src/se/chalmers/watchme/activity/SearchableActivity.java
@@ -1,7 +1,35 @@
 package se.chalmers.watchme.activity;
 
-import android.app.Activity;
+import se.chalmers.watchme.R;
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
 
-public class SearchableActivity extends Activity {
+public class SearchableActivity extends ListActivity {
 
+	/*
+	 * 1. Receiving the Query
+	 * 
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.movie_list_fragment_view);
+		// TODO Create R.layout.search?
+
+		// Get the intent, verify the action and get the query
+		Intent intent = getIntent();
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			doMySearch(query);
+		}
+	}
+
+	private void doMySearch(String query) {
+		// TODO Auto-generated method stub
+		
+	}
 }
