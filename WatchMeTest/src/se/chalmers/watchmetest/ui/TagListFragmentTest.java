295992f92645a80f40b7974095679ff8239b510d
Mattias Henriksson <henriksson.mj@gmail.com>
2012-10-19 20:40:01 +0200

    Add new test in TagListFragment: testDeleteTag()

--- WatchMeTest/src/se/chalmers/watchmetest/ui/TagListFragmentTest.java
+++ WatchMeTest/src/se/chalmers/watchmetest/ui/TagListFragmentTest.java
@@ -1,8 +1,8 @@
 package se.chalmers.watchmetest.ui;
 
 import se.chalmers.watchme.R;
-import se.chalmers.watchme.activity.AddMovieActivity;
 import se.chalmers.watchme.activity.MainActivity;
+import se.chalmers.watchme.activity.TagMovieListActivity;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.jayway.android.robotium.solo.Solo;
@@ -33,8 +33,7 @@ public class TagListFragmentTest extends
 		solo.finishOpenedActivities();
 	}
 	
-	public void testAddTag() {
-		
+	public void testAddTag() {	
 		solo.clickOnText("Movies");
 		solo.clickOnActionBarItem(R.id.menu_add_movie);
 		solo.enterText(TITLE_FIELD, "Batman");
@@ -52,19 +51,48 @@ public class TagListFragmentTest extends
 		solo.enterText(TAG_FIELD, "Action");
 		solo.clickOnButton(ADD_MOVIE_BUTTON);
 		
+		solo.clickOnText("Movies");
 		solo.clickOnActionBarItem(R.id.menu_add_movie);
-		solo.enterText(0, "Dead poet society");
-		solo.enterText(1, "Drama");
+		solo.enterText(TITLE_FIELD, "Dead poet society");
+		solo.enterText(TAG_FIELD, "Drama");
 		solo.clickOnButton(ADD_MOVIE_BUTTON);
 		
 		solo.clickOnText("Tags");
 		solo.clickOnText("drama");
 		
-		solo.assertCurrentActivity("MainActivity expected", MainActivity.class);
+		solo.assertCurrentActivity("TagMovieListActivity expected", TagMovieListActivity.class);
 		boolean wasDramaMoviesFound = solo.searchText("Dead poet society");
 		boolean wasActionMoviesFound = solo.searchText("Batman");
 		assertTrue(wasDramaMoviesFound);
 		assertFalse(wasActionMoviesFound);
 	}
+	
+	public void testDeleteTag() {
+		solo.clickOnText("Movies");
+		solo.clickOnActionBarItem(R.id.menu_add_movie);
+		solo.enterText(TITLE_FIELD, "Batman");
+		solo.enterText(TAG_FIELD, "Action");
+		solo.clickOnButton(ADD_MOVIE_BUTTON);
+		
+		solo.clickOnText("Tags");
+		solo.clickLongOnText("action");
+		
+		boolean dialogAppeared = solo
+				.searchText("Are you sure you want to delete \"action\"?");
+		assertTrue(dialogAppeared);
+		
+		solo.clickOnText("Cancel");
+		boolean expected = true;
+		boolean actual = solo.searchText("action");
+		assertEquals("action-tag was not found", expected, actual);
+		
+		solo.clickLongOnText("action");
+		solo.clickOnText("Yes");
+		expected = true;
+		actual = solo.searchText("action");
+		assertEquals("action-tag was found", expected, actual);
+	}
+	
+	
 
 }
