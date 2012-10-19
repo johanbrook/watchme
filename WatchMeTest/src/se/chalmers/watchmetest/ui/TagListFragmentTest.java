c98dd125c9374924e386389faea14a3acc4eff15
Mattias Henriksson <henriksson.mj@gmail.com>
2012-10-19 20:16:15 +0200

    Add new test in TagListFragment: testClickTag()

--- WatchMeTest/src/se/chalmers/watchmetest/ui/TagListFragmentTest.java
+++ WatchMeTest/src/se/chalmers/watchmetest/ui/TagListFragmentTest.java
@@ -9,6 +9,11 @@ import com.jayway.android.robotium.solo.Solo;
 
 public class TagListFragmentTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
+	
+	// TODO: make public static?
+	final int ADD_MOVIE_BUTTON = 1;
+	final int TITLE_FIELD = 0;
+	final int TAG_FIELD = 1;
 
 	Solo solo;
 
@@ -29,16 +34,37 @@ public class TagListFragmentTest extends
 	}
 	
 	public void testAddTag() {
-		final int ADD_MOVIE_BUTTON = 1;
 		
 		solo.clickOnText("Movies");
 		solo.clickOnActionBarItem(R.id.menu_add_movie);
-		solo.enterText(0, "Batman");
-		solo.enterText(1, "Action");
+		solo.enterText(TITLE_FIELD, "Batman");
+		solo.enterText(TAG_FIELD, "Action");
 		solo.clickOnButton(ADD_MOVIE_BUTTON);
 		solo.clickOnText("Tags");
 		boolean wasTagFound = solo.searchText("action");
 		assertTrue(wasTagFound);
+	}
+
+	public void testClickTag() {
+		solo.clickOnText("Movies");
+		solo.clickOnActionBarItem(R.id.menu_add_movie);
+		solo.enterText(TITLE_FIELD, "Batman");
+		solo.enterText(TAG_FIELD, "Action");
+		solo.clickOnButton(ADD_MOVIE_BUTTON);
+		
+		solo.clickOnActionBarItem(R.id.menu_add_movie);
+		solo.enterText(0, "Dead poet society");
+		solo.enterText(1, "Drama");
+		solo.clickOnButton(ADD_MOVIE_BUTTON);
+		
+		solo.clickOnText("Tags");
+		solo.clickOnText("drama");
+		
+		solo.assertCurrentActivity("MainActivity expected", MainActivity.class);
+		boolean wasDramaMoviesFound = solo.searchText("Dead poet society");
+		boolean wasActionMoviesFound = solo.searchText("Batman");
+		assertTrue(wasDramaMoviesFound);
+		assertFalse(wasActionMoviesFound);
 	}
 
 }
