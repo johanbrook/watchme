611bef23a45b34d3d958c729b5ca486d159c9310
Lisa Stenberg <lisast@student.chalmers.se>
2012-10-19 15:08:06 +0200

    Removed common methods from MovieListFragment, TagListFragment and put them in ContentListFragment

--- src/se/chalmers/watchme/ui/MovieListFragment.java
+++ src/se/chalmers/watchme/ui/MovieListFragment.java
@@ -1,19 +1,15 @@
 package se.chalmers.watchme.ui;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.ResponseCache;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.Calendar;
 
 import se.chalmers.watchme.R;
-import se.chalmers.watchme.activity.AddMovieActivity;
 import se.chalmers.watchme.activity.MovieDetailsActivity;
 import se.chalmers.watchme.database.DatabaseAdapter;
+import se.chalmers.watchme.database.GenericCursorLoader;
+import se.chalmers.watchme.database.ICursorHelper;
 import se.chalmers.watchme.database.MoviesTable;
-import se.chalmers.watchme.database.MyCursorLoader;
 import se.chalmers.watchme.database.WatchMeContentProvider;
 import se.chalmers.watchme.model.Movie;
 import se.chalmers.watchme.net.ImageDownloadTask;
@@ -26,21 +22,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v4.content.Loader.ForceLoadContentObserver;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -50,7 +37,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -59,28 +45,28 @@ import android.widget.Toast;
 
 // TODO Important! API level required does not match with what is used
 @TargetApi(11)
-public class MovieListFragment extends MyListFragment {
+public class MovieListFragment extends ContentListFragment {
 	
-	private SimpleCursorAdapter adapter;
 	private DatabaseAdapter db;
-	private MyCursorLoader cursorLoader;
+	private GenericCursorLoader cursorLoader;
 	
 	private AsyncTask<String, Void, Bitmap> imageTask;
 	private Long tagId;
 	
-	// TODO combine!
-	private String orderBy = MoviesTable.COLUMN_DATE;
+	private static final String orderByDate = MoviesTable.COLUMN_DATE;
+	private static final String orderByTitle = MoviesTable.COLUMN_TITLE;
+	private static final String orderByRating = MoviesTable.COLUMN_RATING + " DESC";
+	
 	private int sortOrder = 0;
 	
 	public MovieListFragment() {
-		super();
+		super(WatchMeContentProvider.CONTENT_URI_MOVIES);
 		this.tagId = (long) -1;
 	}
 
 	public MovieListFragment(Long tagId) {
-		super();
+		super(WatchMeContentProvider.CONTENT_URI_MOVIES);
 		this.tagId = tagId;
-		
 	}
 	
 	@Override
@@ -89,92 +75,11 @@ public class MovieListFragment extends MyListFragment {
 		
 		// TODO: Has to be done in onCreate instead?
 		setHasOptionsMenu(true);
-		Thread.currentThread().setContextClassLoader(getActivity().getClassLoader());
-		
-		// We want to participate in customizing the Action bar options menu
-		this.setHasOptionsMenu(true);
 		
 		final File cacheDir = getActivity().getBaseContext().getCacheDir();
 		ResponseCache.setDefault(new ImageCache(cacheDir));
 
-		String[] from = new String[] { 
-				MoviesTable.COLUMN_MOVIE_ID, 
-				MoviesTable.COLUMN_TITLE,  
-				MoviesTable.COLUMN_RATING ,
-				MoviesTable.COLUMN_DATE,
-				MoviesTable.COLUMN_POSTER_SMALL
-				};
-		
-		int[] to = new int[] { 0 , 
-				R.id.title, 
-				R.id.raiting, 
-				R.id.date,
-				R.id.poster};
-		
-		getActivity().getSupportLoaderManager().initLoader(2, null, this);
-		adapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item_movie , null, from, to, 0);
-		
-		/**
-		 * Convert date text from millis to dd-mm-yyyy format
-		 */
-		//TODO: Refactor?
-		adapter.setViewBinder(new ViewBinder() {
-			
-			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-				
-				if (columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_DATE)) {
-					String date = cursor.getString(columnIndex);
-					TextView textView = (TextView) view;
-					Calendar cal = Calendar.getInstance();
-					cal.setTimeInMillis(Long.parseLong(date));
-					String formattedDate = DateTimeUtils.toSimpleDate(cal);
-					
-					textView.setText(formattedDate);
-					return true;
-				}
-				
-				/*
-				 * Handle rating bar conversion
-				 */
-				else if (columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_RATING)) {
-					int rating = cursor.getInt(columnIndex);
-					RatingBar bar = (RatingBar) view;
-					bar.setRating(rating);
-					
-					return true;
-				}
-				
-				/*
-				 * Handle poster images
-				 */
-				
-				else if(columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_POSTER_SMALL)) {
-					String smallImageUrl = cursor.getString(columnIndex);
-					final ImageView imageView = (ImageView) view;
-					
-					if(smallImageUrl != null && !smallImageUrl.isEmpty()) {
-						
-						// Fetch the image in an async task
-						imageTask = new ImageDownloadTask(new ImageDownloadTask.TaskActions() {
-							
-							public void onFinished(Bitmap image) {
-								if(image != null) {
-									((ImageView) imageView).setImageBitmap(image);
-								}
-							}
-						});
-						
-						imageTask.execute(new String[]{smallImageUrl});
-					}
-					
-					return true;
-				}
-				
-				return false;
-			}
-		});
-		
-		setListAdapter(adapter);
+		setUpAdapter();
 	    
 		// Set up listeners to delete and view a movie
         this.getListView().setOnItemClickListener(new OnDetailsListener());
@@ -211,24 +116,29 @@ public class MovieListFragment extends MyListFragment {
 	
 	@Override
 	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		db = new DatabaseAdapter(getActivity().getContentResolver());
 		
-		return new MyCursorLoader(getActivity(), 
-				WatchMeContentProvider.CONTENT_URI_MOVIES,tagId,orderBy);
-	}
+			return new GenericCursorLoader(getActivity(), new ICursorHelper() {
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> arg0, Cursor cursor) {
-		System.out.println("MLF: onLoadFinished: count: " + cursor.getCount());
-		adapter.swapCursor(cursor);
-		adapter.notifyDataSetChanged();
-	}
+			@Override
+			public Uri getUri() {
+				return MovieListFragment.this.getUri();
+			}
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> arg0) {
-		// data is not available anymore, delete reference
-	    adapter.swapCursor(null);
-	    adapter.notifyDataSetChanged();
-		
+			@Override
+			public String getSortOrder() {
+				return orderByDate;
+			}
+
+			@Override
+			public Cursor getCursor() {
+				if (tagId == -1) {
+					return db.getAllMoviesCursor(getSortOrder());
+				}
+				return db.getAttachedMovies(tagId);
+			}
+			
+		});
 	}
 	
     @Override
@@ -258,39 +168,31 @@ public class MovieListFragment extends MyListFragment {
     			new DialogInterface.OnClickListener() {
     			public void onClick(DialogInterface dialog, int item) {
 
+    				Cursor cursor = null;
     				switch(item) {
     				case 0:
-    					orderBy = MoviesTable.COLUMN_DATE;
+    					cursor = db.getAllMoviesCursor(orderByDate);
     					break;
     				case 1:
-    					orderBy = MoviesTable.COLUMN_RATING + " DESC";
+    					cursor = db.getAllMoviesCursor(orderByRating);
     					break;
     				case 2:
-    					orderBy = MoviesTable.COLUMN_TITLE;
+    					cursor = db.getAllMoviesCursor(orderByTitle);
     					break;
     				default:
     					break;
     				}
     				sortOrder = item;
     				// Change the cursor
-    				Cursor cursor = db.getAllMoviesCursor(orderBy);
+    				
     				onLoadFinished(cursorLoader, cursor);
-    				adapter.notifyDataSetChanged();
+    				getAdapter().notifyDataSetChanged();
     				
     				dialog.dismiss();
     			}
     			});
         
         alertbox.show();
-    }
-    
-    @Override
-    public void filter(String search) {
-    	db = new DatabaseAdapter(getActivity().getContentResolver());
-    	Cursor cursor = db.searchForMovies(search);
-		
-		onLoadFinished(cursorLoader, cursor);
-		adapter.notifyDataSetChanged();
     }
 	
 	/**
@@ -360,5 +262,84 @@ public class MovieListFragment extends MyListFragment {
             alertbox.show();
 			return true;
 		}    	
+	}
+
+	private void setUpAdapter() {
+		String[] from = new String[] { 
+				MoviesTable.COLUMN_MOVIE_ID, 
+				MoviesTable.COLUMN_TITLE,  
+				MoviesTable.COLUMN_RATING ,
+				MoviesTable.COLUMN_DATE,
+				MoviesTable.COLUMN_POSTER_SMALL
+				};
+		
+		int[] to = new int[] { 0 , 
+				R.id.title, 
+				R.id.raiting, 
+				R.id.date,
+				R.id.poster};
+		
+		getActivity().getSupportLoaderManager().initLoader(2, null, this);
+		setAdapter(new SimpleCursorAdapter(getActivity(), R.layout.list_item_movie , null, from, to, 0));
+		
+		/**
+		 * Convert date text from millis to dd-mm-yyyy format
+		 */
+		//TODO: Refactor?
+		getAdapter().setViewBinder(new ViewBinder() {
+			
+			public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+				
+				if (columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_DATE)) {
+					String date = cursor.getString(columnIndex);
+					TextView textView = (TextView) view;
+					Calendar cal = Calendar.getInstance();
+					cal.setTimeInMillis(Long.parseLong(date));
+					String formattedDate = DateTimeUtils.toSimpleDate(cal);
+					
+					textView.setText(formattedDate);
+					return true;
+				}
+				
+				/*
+				 * Handle rating bar conversion
+				 */
+				else if (columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_RATING)) {
+					int rating = cursor.getInt(columnIndex);
+					RatingBar bar = (RatingBar) view;
+					bar.setRating(rating);
+					
+					return true;
+				}
+				
+				/*
+				 * Handle poster images
+				 */
+				
+				else if(columnIndex == cursor.getColumnIndexOrThrow(MoviesTable.COLUMN_POSTER_SMALL)) {
+					String smallImageUrl = cursor.getString(columnIndex);
+					final ImageView imageView = (ImageView) view;
+					
+					if(smallImageUrl != null && !smallImageUrl.isEmpty()) {
+						
+						// Fetch the image in an async task
+						imageTask = new ImageDownloadTask(new ImageDownloadTask.TaskActions() {
+							
+							public void onFinished(Bitmap image) {
+								if(image != null) {
+									((ImageView) imageView).setImageBitmap(image);
+								}
+							}
+						});
+						
+						imageTask.execute(new String[]{smallImageUrl});
+					}
+					
+					return true;
+				}
+				
+				return false;
+			}
+		});		
 	}
 }
--- src/se/chalmers/watchme/ui/TagListFragment.java
+++ src/se/chalmers/watchme/ui/TagListFragment.java
@@ -4,60 +4,47 @@ import se.chalmers.watchme.R;
 import se.chalmers.watchme.activity.MainActivity;
 import se.chalmers.watchme.activity.TagMovieListActivity;
 import se.chalmers.watchme.database.DatabaseAdapter;
+import se.chalmers.watchme.database.ICursorHelper;
 import se.chalmers.watchme.database.TagsTable;
-import se.chalmers.watchme.database.MyCursorLoader;
+import se.chalmers.watchme.database.GenericCursorLoader;
 import se.chalmers.watchme.database.WatchMeContentProvider;
 import se.chalmers.watchme.model.Tag;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.text.method.KeyListener;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.Filter;
 import android.widget.ListView;
-import android.widget.PopupMenu;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemLongClickListener;
 
-public class TagListFragment extends MyListFragment {
+public class TagListFragment extends ContentListFragment {
 	
-	private SimpleCursorAdapter adapter;
 	private DatabaseAdapter db;
-	private MyCursorLoader cursorLoader;
+	
+	public TagListFragment() {
+		super(WatchMeContentProvider.CONTENT_URI_TAGS);
+	}
 	
 	@Override
 	public void onActivityCreated(Bundle b) {
 		super.onActivityCreated(b);
-		Thread.currentThread().setContextClassLoader(getActivity().getClassLoader());
-
-		String[] from = new String[] { TagsTable.COLUMN_TAG_ID, TagsTable.COLUMN_NAME };
-		int[] to = new int[] { android.R.id.text1 , android.R.id.text1 };
 		
-		getActivity().getSupportLoaderManager().initLoader(1, null, this);
-		adapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_1 , null, from, to, 0);
-		setListAdapter(adapter);
+		// We want to participate in manipulating the Action bar options menu
+		// TODO But we don't do it?
+		this.setHasOptionsMenu(true);
+		
+		setUpAdapter();
 		
 		// Set up listener to delete a Tag.
 		this.getListView().setOnItemLongClickListener(new OnDeleteListener());
-		
-		// We want to participate in manipulating the Action bar options menu
-		this.setHasOptionsMenu(true);
 	}
 	
 	@Override
@@ -67,36 +54,27 @@ public class TagListFragment extends MyListFragment {
 		return inflater.inflate(R.layout.tag_list_fragment_view, container, false);
 	}
 	
-	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
-		Long tagId = (long) -1;
-		
-		cursorLoader = new MyCursorLoader(getActivity(), 
-				WatchMeContentProvider.CONTENT_URI_TAGS,
-				tagId,null);
-		
-		return cursorLoader;
-	}
-
-	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
-		
-		adapter.swapCursor(arg1);	
-		adapter.notifyDataSetChanged();
-	}
-
-	public void onLoaderReset(Loader<Cursor> arg0) {
-		// data is not available anymore, delete reference
-	    adapter.swapCursor(null);
-	    adapter.notifyDataSetChanged();
-		
-	}
-	
 	@Override
-	public void filter(String search) {
-		db = new DatabaseAdapter(this.getActivity().getContentResolver());
-		Cursor cursor = db.searchForTags(search);
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		db = new DatabaseAdapter(getActivity().getContentResolver());
 		
-		onLoadFinished(cursorLoader, cursor);
-		adapter.notifyDataSetChanged();
+		return new GenericCursorLoader(getActivity(), new ICursorHelper() {
+			
+			@Override
+			public Uri getUri() {
+				return TagListFragment.this.getUri();
+			}
+			
+			@Override
+			public String getSortOrder() {
+				return null;
+			}
+			
+			@Override
+			public Cursor getCursor() {
+				return db.getAllTagsCursor();
+			}
+		});
 	}
 	
 	@Override
@@ -148,5 +126,13 @@ public class TagListFragment extends MyListFragment {
             alertbox.show();
 			return true;
 		}    	
+	}
+
+	private void setUpAdapter() {
+		String[] from = new String[] { TagsTable.COLUMN_TAG_ID, TagsTable.COLUMN_NAME };
+		int[] to = new int[] { android.R.id.text1 , android.R.id.text1 };
+		
+		getActivity().getSupportLoaderManager().initLoader(1, null, this);
+		super.setAdapter(new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_1 , null, from, to, 0));
 	}
 }
