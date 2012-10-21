07feb04e750effdafaab1c879fcc5848f3c219c1
Johan Brook <johan.jvb@gmail.com>
2012-10-21 11:58:44 +0200

    Fix: Don't create a notification if a movie wasn't added correctly

---
+++ src/se/chalmers/watchme/database/MovieAlreadyExistsException.java
@@ -0,0 +1,41 @@
+package se.chalmers.watchme.database;
+
+import se.chalmers.watchme.model.Movie;
+
+/**
+*	MovieAlreadyExistsException.java
+*
+*	Exception to throw when a movie already exist in the database
+*	(in reference to a movie's title).
+*
+*	@author Johan Brook
+*	@copyright (c) 2012 Johan Brook
+*	@license MIT
+*/
+
+public class MovieAlreadyExistsException extends Exception {
+	
+	private Movie movie;
+
+	/**
+	 * Create a new movie exception.
+	 * 
+	 * @param detailMessage The message string
+	 * @param movie The incorrect movie
+	 */
+	public MovieAlreadyExistsException(String detailMessage, Movie movie) {
+		super(detailMessage);
+		
+		this.movie = movie;
+	}
+	
+	/**
+	 * Get the incorrect movie object.
+	 * 
+	 * @return The duplicated movie
+	 */
+	public Movie getMovie() {
+		return this.movie;
+	}
+
+}
