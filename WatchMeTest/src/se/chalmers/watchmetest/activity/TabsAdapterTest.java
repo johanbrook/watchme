519400236d24274a94dedbeef1f488bcf770491a
Mattias Henriksson <henriksson.mj@gmail.com>
2012-10-21 15:09:22 +0200

    Add comments in TabsAdapterTest class

--- WatchMeTest/src/se/chalmers/watchmetest/activity/TabsAdapterTest.java
+++ WatchMeTest/src/se/chalmers/watchmetest/activity/TabsAdapterTest.java
@@ -10,6 +10,12 @@ import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 
+/**
+ * Class that tests functionality implemented in the TabsAdapter class.
+ * 
+ * @author mattiashenriksson
+ * 
+ */
 public class TabsAdapterTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -20,12 +26,12 @@ public class TabsAdapterTest extends
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		// TODO: Add mainActivity, actionBar, etc., as instance variables?
-		// Lesser repeating of code but bad reusing same stuff for different
-		// tests? (think Joacim von Hacht said that)...
 	}
 
 	// TODO fix api level
+	/**
+	 * Test if it's possible to add to the tabs adapter
+	 */
 	@TargetApi(11)
 	@UiThreadTest
 	public void testAddTab() {
@@ -44,6 +50,7 @@ public class TabsAdapterTest extends
 
 		int randomNr = (int) (Math.random() * 10 + 1);
 
+		// Create and add random number of tabs
 		for (int i = 1; i <= randomNr; i++) {
 			Tab tab = actionBar.newTab().setText("tab" + i);
 			tabsAdapter.addTab(tab, MovieListFragment.class, null);
@@ -60,6 +67,9 @@ public class TabsAdapterTest extends
 	}
 
 	// TODO: fix api level
+	/**
+	 * Test if method changes the currently viewed tab
+	 */
 	@TargetApi(11)
 	@UiThreadTest
 	public void testOnTabSelected() {
@@ -68,6 +78,7 @@ public class TabsAdapterTest extends
 		ViewPager viewPager = new ViewPager(mainActivity);
 		TabsAdapter tabsAdapter = new TabsAdapter(mainActivity, viewPager);
 
+		// Create and add two tabs
 		Tab tab1 = actionBar.newTab().setText("tab1");
 		tabsAdapter.addTab(tab1, MovieListFragment.class, null);
 
@@ -78,6 +89,9 @@ public class TabsAdapterTest extends
 		// and
 		// use assertTrue(viewPager.getCurrentItem() ==
 		// tabsAdapter.getTabs().indexOf(tab2))
+
+		// Check that the view pager is changing viewed tab and viewing the
+		// correct one
 		tabsAdapter.onTabSelected(tab2, null);
 		assertTrue(viewPager.getCurrentItem() == 1);
 
@@ -87,6 +101,9 @@ public class TabsAdapterTest extends
 	}
 
 	// TODO: fix api level
+	/**
+	 * Test that getItem() returns correct values
+	 */
 	@TargetApi(11)
 	@UiThreadTest
 	public void testGetItem() {
@@ -94,6 +111,8 @@ public class TabsAdapterTest extends
 		ActionBar actionBar = mainActivity.getActionBar();
 		TabsAdapter tabsAdapter = new TabsAdapter(mainActivity, new ViewPager(
 				mainActivity));
+
+		// Create and add tab
 		Tab tab = actionBar.newTab().setText("tab");
 		tabsAdapter.addTab(tab, MovieListFragment.class, null);
 
@@ -101,6 +120,10 @@ public class TabsAdapterTest extends
 	}
 
 	// TODO: fix api level
+	/**
+	 * Test that getCount() is returning the current number of tabs added to the
+	 * TabsAdapter
+	 */
 	@TargetApi(11)
 	@UiThreadTest
 	public void testGetCount() {
@@ -111,6 +134,7 @@ public class TabsAdapterTest extends
 
 		int randomNr = (int) (Math.random() * 10 + 1);
 
+		// Add random number of tabs
 		for (int i = 0; i < randomNr; i++) {
 			Tab tab = actionBar.newTab().setText("tab");
 			tabsAdapter.addTab(tab, MovieListFragment.class, null);
