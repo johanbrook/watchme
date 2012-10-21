43664bdae672543465f84d72d0ae1fd66938d1c0
Mattias Henriksson <henriksson.mj@gmail.com>
2012-10-21 17:13:03 +0200

    Add licens for files in activity-package

--- src/se/chalmers/watchme/activity/MainActivity.java
+++ src/se/chalmers/watchme/activity/MainActivity.java
@@ -1,3 +1,15 @@
+/**
+ *	MainActivity.java
+ *
+ *	The Main Activity for the WatchMe application. Contains to tabs: one
+ *   presenting all movies and one presenting all tags.
+ *
+ *	@author lisastenberg
+ *	@author Johan Brook
+ *	@copyright (c) 2012 Johan Brook, Robin Andersson, Lisa Stenberg, Mattias Henriksson
+ *	@license MIT
+ */
+
 package se.chalmers.watchme.activity;
 
 import java.util.List;
@@ -18,50 +30,47 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
 
-/**
- * The Main Activity for the WatchMe application. Contains to tabs: one
- * presenting all movies and one presenting all tags.
- * 
- * @author lisastenberg
- */
 public class MainActivity extends FragmentActivity {
-	
+
 	public static final String MOVIE_DETAILS_ID = "se.chalmers.watchme.DETAILS_ID";
 	public static final String MOVIE_DETAILS_TITLE = "se.chalmers.watchme.DETAILS_TITLE";
 	public static final String MOVIE_DETAILS_RATING = "se.chalmers.watchme.DETAILS_RATING";
 	public static final String MOVIE_DETAILS_NOTE = "se.chalmers.watchme.DETAILS_NOTE";
-	
+
 	private ViewPager viewPager;
 	private TabsAdapter tabsAdapter;
 	private ActionBar actionBar;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        //setup view pager
-        this.viewPager = new ViewPager(this);
-        this.viewPager.setId(R.id.vPager);
-        setContentView(viewPager);
-        
-        //setup actionbar
-        actionBar = getActionBar();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Setup view pager
+		this.viewPager = new ViewPager(this);
+		this.viewPager.setId(R.id.vPager);
+		setContentView(viewPager);
+
+		// Setup action bar
+		actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		actionBar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
-		
-		//setup tabs
+
+		// Setup tabs
 		tabsAdapter = new TabsAdapter(this, viewPager);
-		tabsAdapter.addTab(actionBar.newTab().setText(R.string.tab_movies), MovieListFragment.class, null);
-		tabsAdapter.addTab(actionBar.newTab().setText(R.string.tab_tags), TagListFragment.class, null);
+		tabsAdapter.addTab(actionBar.newTab().setText(R.string.tab_movies),
+				MovieListFragment.class, null);
+		tabsAdapter.addTab(actionBar.newTab().setText(R.string.tab_tags),
+				TagListFragment.class, null);
 		if (savedInstanceState != null) {
-			actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
-		}	
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        
+			actionBar.setSelectedNavigationItem(savedInstanceState.getInt(
+					"tab", 0));
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+
 		/*
 		 * Add necessary functionality for the search widget
 		 */
@@ -73,57 +82,61 @@ public class MainActivity extends FragmentActivity {
 		searchView.setIconifiedByDefault(false); // Do not iconify the widget;
 													// expand it by default
 
-        return true;
-    }
-    
-    /**
-     * When the user clicks on a button in the Action bar.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        
-    	switch(item.getItemId()) {
-    	case R.id.menu_add_movie:
-    		Intent intent = new Intent(this, AddMovieActivity.class);
-            startActivity(intent);
-            return true;
-    		
-    	case R.id.menu_send_email_button:
-    		sendEmail();
-    		return true;
-    		
-    	case R.id.menu_search_button:
-            onSearchRequested();
-            return true;
-            
-    	default:
-    		return super.onOptionsItemSelected(item);
-    	}
-    }
-    
-    private void sendEmail() {
-    	Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-    	emailIntent.setType("text/plain");
-    	
-    	List<Movie> movies = new DatabaseAdapter(getContentResolver()).getAllMovies();
-    	
-    	// Parse all movies with their dates:
-    	String movieString = "";
-    	for(Movie m : movies) {
-    		movieString += m.getTitle() + " ("+DateTimeUtils.toSimpleDate(m.getDate()) + ")\n";
-    	}
-    	
-    	emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, R.string.email_subject);
-    	emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, movieString);  
-    	
-    	// Let the user choose email app to mail from
-    	startActivity(Intent.createChooser(emailIntent, getString(R.string.choose_message_app)));
-    }
-        
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
-    }
+		return true;
+	}
+
+	/**
+	 * When the user clicks on a button in the Action bar.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.menu_add_movie:
+			Intent intent = new Intent(this, AddMovieActivity.class);
+			startActivity(intent);
+			return true;
+
+		case R.id.menu_send_email_button:
+			sendEmail();
+			return true;
+
+		case R.id.menu_search_button:
+			onSearchRequested();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void sendEmail() {
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+		emailIntent.setType("text/plain");
+
+		List<Movie> movies = new DatabaseAdapter(getContentResolver())
+				.getAllMovies();
+
+		// Parse all movies with their dates:
+		String movieString = "";
+		for (Movie m : movies) {
+			movieString += m.getTitle() + " ("
+					+ DateTimeUtils.toSimpleDate(m.getDate()) + ")\n";
+		}
+
+		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+				R.string.email_subject);
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, movieString);
+
+		// Let the user choose email app to mail from
+		startActivity(Intent.createChooser(emailIntent,
+				getString(R.string.choose_message_app)));
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+	}
 
 }
